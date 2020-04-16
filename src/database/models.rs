ee2c0c1effed894876d3740aa0f8de12dc62ad89
Connor <connor.postma@gmail.com>
2020-04-16 14:01:32 -0400

    added models for the new database tables

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,5 +1,7 @@
 use serde::{Serialize, Deserialize};
-use super::schema::users;
+use super::schema::{users, api_users};
+
+// Users DB Models
 
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct User {
@@ -17,4 +19,22 @@ pub struct User {
 pub struct NewUser<'a> {
     pub display_name: &'a str,
     pub steamid2: &'a str,
+}
+
+// API Authorization Users Models
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct ApiUser {
+    pub user_id: i32,
+    pub username: String,
+    pub password: Option<String>,
+    pub auth_ip: Option<String>,
+}
+
+#[derive(Insertable)]
+#[table_name="api_users"]
+pub struct NewApiUser<'a> {
+    pub username: &'a str,
+    pub password: &'a str,
+    pub auth_ip: &'a str,
 }
