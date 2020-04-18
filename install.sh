90e24e7e1e66d3a57dc5cd116d8ea7174bf13da1
Connor <connor.postma@gmail.com>
2020-04-18 16:01:04 -0400

    added the correct library that needs to be installed for usage of this app on ubuntu

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #puts the daemon into the proper location with the proper configuration values
 #root is required to run this
+
+# Install on ubuntu the required library for talking to mysql
+sudo apt-get install libmariadb-dev
