c1be47f498ba7c1f820f4508107b2bc4a3672f8f
Connor <connor.postma@gmail.com>
2020-04-23 20:45:44 -0400

    modified query to make it return a min-rounds-played leaderboard

--- src/database/sql/getrank.sql
+++ src/database/sql/getrank.sql
@@ -1,8 +1,8 @@
-SELECT id, display_name, rws, FIND_IN_SET( rws, (
+SELECT id, display_name, rws, rounds_total, FIND_IN_SET( rws, (
 SELECT GROUP_CONCAT( rws
 ORDER BY rws DESC ) 
-FROM users )
+FROM users WHERE rounds_total > 50 )
 ) AS rank
 FROM users
-WHERE id = 1
+WHERE rounds_total > 50
 ORDER BY rank
