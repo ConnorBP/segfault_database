01d0eed9f34b4cadb49a7ab90dd3bf1ba5e4979a
Connor <connor.postma@gmail.com>
2020-04-25 02:00:44 -0400

    created pm2 launch script to use with ecosystem config

--- scripts/ecosystem.config.example.js
+++ scripts/ecosystem.config.example.js
@@ -3,6 +3,7 @@ module.exports = {
         {
           name: "SegfaultDatabase",
           script: "./sfdb_run.sh",
+          interpreter: "bash",
           watch: true,
           env: {
               "DATABASE_URL": "mysql://segfault_users:password@127.0.0.1:3306/segfault_users",
---
+++ scripts/sfdb_pm2_env.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo ""
+echo "SegfaultDatabase pm2 Startup. Please ensure you have installed pm2/NodeJS/npm with the installer script first! Running SegfaultDatabase with pm2 means that pm2 runs SegfaultDatabase in the background of your machine and auto-restart even after reboot. If you are running the bot already, you can close the session you are currently using and start SegfaultDatabase with this method."
+
+echo ""
+echo ""
+root=$(pwd)
+    
+echo ""
+cd "$root"
+echo "Starting sfdb in pm2 with auto-restart and no auto-update..."
+sudo pm2 start "$root/ecosystem.config.js"
+sudo pm2 startup
+sudo pm2 save
+echo ""
+echo "If you did everything correctly, pm2 should have started up sfdb! Please use sudo pm2 info SegfaultDatabase to check. You can view pm2 logs with sudo pm2 logs SegfaultDatabase"
